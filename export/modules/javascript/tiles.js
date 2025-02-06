dd54ca46c1e8a8f3f5ec5fabbde5182366a45ab0
MarcelEindhoven <Marcel.Eindhoven@Hotmail.com>
2025-02-06 09:40:33 +0100

    tile paint method implemented

--- export/modules/javascript/tiles.js
+++ export/modules/javascript/tiles.js
@@ -3,6 +3,8 @@ define(['dojo/_base/declare'], (declare) => {
         /**
          * Dependencies:
          * dojo
+         * document
+         * game
          */
         /**
          * Use case creation:
@@ -15,18 +17,49 @@ define(['dojo/_base/declare'], (declare) => {
                 this[property] = properties[property];
             }
         },
-        create_tile_from(tile_specification) {
-            class tile {
+        /**
+         * specification:
+         * tile_id
+         * stage
+         * horizontal
+         * vertical
+         */
+        create_tile_from(specification) {
+            class Tile {
                 constructor(dependencies) {
                     this.clone(dependencies);
+                }
+                create_token(specification) {
+                    this.clone(specification);
+                    this.unique_id = "tile-" + this.tile_id;
+                    this.document.getElementById('game_play_area').insertAdjacentHTML('beforeend', `
+                        <div id="${this.unique_id}">
+                    `);
+                    this.dojo.addClass(this.unique_id,'tile');
+                    const id_horizontal = this.tile_id % 20;
+                    const id_vertical = (this.tile_id-id_horizontal) / 20;
+                    this.dojo.style(this.unique_id, 'backgroundPosition', '-' + 80 * id_horizontal + 'px -' + 80 * id_vertical + 'px');
                 }
                 clone(properties){
                     for (var property in properties) {
                         this[property] = properties[property];
                     }
-                }        
+                }
+                move_to(element_id, x, y) {
+                    this.element_id = element_id;
+                    this.x = x;
+                    this.y = y;
+                }
+                /**
+                 * Precondition: move_to has been called
+                 */
+                paint() {
+                    this.game.placeOnObjectPos(this.unique_id, this.element_id, this.x, this.y);
+                }
             }
-            return new tile(tile_specification);
+            result = new Tile({document: this.document, dojo: this.dojo, game: this.game,});
+            result.create_token(specification);
+            return result;
         },
         /**
          */
--- test/test_tiles.js
+++ test/test_tiles.js
@@ -3,24 +3,52 @@ var sinon = require('sinon');
 
 var sut_module = require('../export/modules/javascript/tiles.js');
 
+class Document {
+    getElementById(element_id) {return {insertAdjacentHTML: sinon.spy(),};}
+}
 describe('Tiles', function () {
     beforeEach(function() {
-        dojo = {connect:sinon.spy(), addClass:sinon.spy(), removeClass:sinon.spy(), };
-        dependencies = {dojo: dojo, stocks:{'next0': sinon.spy(), 'quarry0': sinon.spy(), 'quarry1': sinon.spy()}};
+        dojo = {style:sinon.spy(), addClass:sinon.spy(), removeClass:sinon.spy(), };
+        document = new Document();
+        game = {placeOnObjectPos:sinon.spy(),};
+        dependencies = {dojo: dojo, document: document, game:game, };
         sut = new sut_module(dependencies);
-        callback_object = {
-            tile_placed: sinon.spy(),
-        };
+        tile_specification = {tile_id: 0, stage: 0, horizontal: 0,vertical: 0,};
+        element_id = 'HTML element ID ';
+        x = 12;
+        y = 33;
 
     });
     describe('Create tile from specification', function () {
         it('copies the input parameters', function () {
             // Arrange
-            tile_specification = {test_parameter: dojo};
+            tile_specification.test_parameter = dojo;
             // Act
             tile = sut.create_tile_from(tile_specification);
             // Assert
             assert.equal(tile.test_parameter, tile_specification.test_parameter);
+        });
+    });
+    describe('Paint', function () {
+        beforeEach(function() {
+            tile = sut.create_tile_from(tile_specification);
+        });
+        it('calls placeOnObjectPos with unique_id', function () {
+            // Arrange
+            // Act
+            tile.paint();
+            // Assert
+            assert.equal(game.placeOnObjectPos.getCall(0).args[0], tile.unique_id);
+        });
+        it('uses the parameters from move', function () {
+            // Arrange
+            tile.move_to(element_id, x, y);
+            // Act
+            tile.paint();
+            // Assert
+            assert.equal(game.placeOnObjectPos.getCall(0).args[1], element_id);
+            assert.equal(game.placeOnObjectPos.getCall(0).args[2], x);
+            assert.equal(game.placeOnObjectPos.getCall(0).args[3], y);
         });
     });
 });
