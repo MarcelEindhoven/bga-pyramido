b20b946e44151df1bc4f663e67c1d130428b4821
MarcelEindhoven <Marcel.Eindhoven@Hotmail.com>
2025-10-09 18:38:55 +0200

    Got licence

--- export/gameinfos.inc.php
+++ export/gameinfos.inc.php
@@ -20,10 +20,10 @@ $gameinfos = [
     'publisher_website' => 'https://www.jeuxsynapsesgames.com/',
 
     // Board Game Geek ID of the publisher
-    'publisher_bgg_id' => 1234,
+    'publisher_bgg_id' => 329484,
 
     // Board game geek ID of the game
-    'bgg_id' => 0,
+    'bgg_id' => 329484,
 
     // Players configuration that can be played (ex: 2 to 4 players)
     'players' => [2, 3, 4],
@@ -37,16 +37,16 @@ $gameinfos = [
     // 'not_recommend_player_number' => array( 2, 3 ),      // <= example: this is not recommended to play this game with 2 or 3 players
 
     // Estimated game duration, in minutes (used only for the launch, afterward the real duration is computed)
-    'estimated_duration' => 60,
+    'estimated_duration' => 45,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = fast)
-    'fast_additional_time' => 30,
+    'fast_additional_time' => 60,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = medium)
-    'medium_additional_time' => 40,
+    'medium_additional_time' => 80,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = slow)
-    'slow_additional_time' => 50,
+    'slow_additional_time' => 100,
 
     // If you are using a tie breaker in your game (using "player_score_aux"), you must describe here
     // the formula used to compute "player_score_aux". This description will be used as a tooltip to explain
@@ -62,9 +62,6 @@ $gameinfos = [
 
     // Allow to rank solo games for games where it's the only available mode (ex: Orchard). Should be left to false for games where solo mode exists in addition to multiple players mode.
     'solo_mode_ranked' => false,
-
-    // Game is "beta". A game MUST set is_beta=1 when published on BGA for the first time, and must remains like this until all bugs are fixed.
-    'is_beta' => 1,
 
     // Is this game cooperative (all players wins together or loose together)
     'is_coop' => 0,
